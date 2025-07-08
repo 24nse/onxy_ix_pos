703e334d55a1ea5cd424a516c0c634ffc763282d
24nse <noohsaeed240@gmail.com>
2025-07-08 12:20:30 +0300

    create  and setup project add initial implementation of app colors, styles

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme
+  static const Color primaryLight = Color(0xFF00529B);
+  static const Color secondaryLight = Color(0xFF00A884);
+  static const Color backgroundLight = Color(0xFFF0F2F5);
+  static const Color cardLight = Color(0xFFFFFFFF);
+  static const Color textPrimaryLight = Color(0xFF111B21);
+  static const Color textSecondaryLight = Color(0xFF667781);
+  static const Color borderLight = Color(0xFFE9EDF0);
+
+  // Dark Theme
+  static const Color primaryDark = Color(0xFF53BDEB);
+  static const Color secondaryDark = Color(0xFF00A884);
+  static const Color backgroundDark = Color(0xFF111B21);
+  static const Color cardDark = Color(0xFF202C33);
+  static const Color textPrimaryDark = Color(0xFFE9EDF0);
+  static const Color textSecondaryDark = Color(0xFF8696A0);
+  static const Color borderDark = Color(0xFF374045);
+
+  // Common Colors
+  static const Color success = Color(0xFF27AE60);
+  static const Color error = Color(0xFFEB5757);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+}
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:onyx_ix_pos/core/utils/app_colors.dart';
+
+abstract class Styles {
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Cairo',
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Cairo',
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Cairo',
+  );
+
+  static const textStyle16Bold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+    fontFamily: 'Cairo',
+  );
+
+  static const textStyle12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Cairo',
+  );
+}
